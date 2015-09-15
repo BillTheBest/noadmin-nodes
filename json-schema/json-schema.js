a9efd87b75c9b19dae640ed7b79626282d5a56f6
Zeb Sampedro <sampedro@colorado.edu>
2015-09-15 13:18:33 -0600

    Basic implememtation of schema nodes.

--- json-schema/json-schema.js
+++ json-schema/json-schema.js
@@ -13,9 +13,30 @@ module.exports = function(RED) {
 
   function JsonSchemaValidator(n) {
     RED.nodes.createNode(this,n);
-    var jsonSchema = n.jsonSchema;
+    var configNode = RED.nodes.getNode(n.jsonSchema);
+
+    this.on('input', function (msg) {
+      var valid = tv4.validate(msg.payload, configNode.schema);
+      if (!valid) {
+        this.error(tv4.error);
+        msg.payload = '';
+      }
+      this.send(msg);
+    });
   }
 
   RED.nodes.registerType("json-schema-validator",JsonSchemaValidator);
+
+  function GetJsonSchema(n) {
+    RED.nodes.createNode(this,n);
+    var configNode = RED.nodes.getNode(n.jsonSchema);
+
+    this.on('input', function(msg) {
+      msg.payload = configNode.schema;
+      this.send(msg);
+    });
+  }
+
+  RED.nodes.registerType("get-json-schema",GetJsonSchema)
 
 }
